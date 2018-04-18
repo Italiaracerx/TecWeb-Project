091acd4dbf931a36c2788d06c03da41d11b0b45b
Daniel Rossi <bovo.96@gmail.com>
2018-04-18 14:11:31 +0200

    modifiche stile pagina admin

--- back-end/mainPage/general_admin.php
+++ back-end/mainPage/general_admin.php
@@ -12,85 +12,105 @@
     $log = new login();
     $lista_utenti =$log->read();
 ?>
-<div id="content">
-        <h3 id="titolo_negozio">GENERALE</h3>
+    <div id="content">
         <div id="sopra">
-        <div class="BoxSuperiore">
-            <p class="intestazione">CREAZIONE NEGOZIO</p>
-            <div id="demo"></div>
-            <form class="NuovoNegozio" action="" method="post" onsubmit="return validateForm(this)">
+
+            <div class="form_sopra">
+              <p class="intestazione">CREAZIONE NEGOZIO</p>
+              <div id="demo"></div>
+              <form  action="" method="post" onsubmit="return validateForm(this)">
                 <div>
                 <label for="nome_negozio">Nome Negozio:</label>
-                <input class="allarga" type="text" id="nome_negozio" name="username" />    
+                <input type="text" id="nome_negozio" name="nome_negozio"/>    
                 <label for="password">Password:</label>
-                <input class="allarga" type="password" id="password" name="password" />
+                <input type="password" id="password" name="password" />
                 <label for="conferma_password">Conferma Password:</label>
-                <input class="allarga" type="password" id="conferma_password" name="Password_1" />                      
-                <input title="Tasto Reset" type="reset" value="Reset"/>  
-                <input title="Tasto Salva" type="submit" value="Salva"/>
+                <input type="password" id="conferma_password" name="conferma_password" />  
+                <div class="invia">                       
+                <input type="reset"  name="tasto_reset"  value="Reset"/>  
+                <input type="submit" name="tasto_salva"  value="Salva"/>
                 </div>
-            </form>
-        </div>
-        <div class="BoxSuperiore">
-            <p class="intestazione">ELIMINA NEGOZIO</p> 
-            <div id="demo_1"></div>
-            <form  class="NuovoNegozio" action="" method="post" onsubmit="return validateUser()">
+                </div>
+               </form>
+             </div>
+
+            <div class="form_sopra">
+              <p class="intestazione">ELIMINA NEGOZIO</p> 
+              <div id="demo_1"></div>
+              <form  action="" method="post" onsubmit="return validateUser()">
                 <div>
-                <label for="negozio">Nome Negozio:</label>
-                <select name="negozio" id="negozio" class="allarga">
-                <option value="Cerca nel menu:">Cerca nel menu:</option>
-                <?php
-                    foreach ($lista_utenti as $utente ){
-                        echo '<option value="'.$utente['username'].'">'.$utente['username'].'</option>';
-                    }
-                ?>
-                </select>               
-                <input title="Tasto Reset" type="reset" value="Reset"/>  
-                <input title="Tasto Salva" type="submit" value="Salva"/>
+                  <label for="elimina_negozio">Nome Negozio:</label>
+                  <select name="nelimina_negozio" id="elimina_negozio" >
+                  <option value="Cerca nel menu:">Cerca nel menu:</option>
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="opel">Opel</option>
+                  <option value="audi">Audi</option>
+                  </select>  
+                  <div class="invia">                
+                  <input type="reset"  name="tasto_reset"  value="Reset"/>  
+                  <input type="submit" name="tasto_salva"  value="Salva"/>
+                  </div>
                 </div>
-            </form>
-        </div>
-    </div>
+               </form>
+            </div>
+
+         </div>
      
-        <div id="sopra">
-        <div class="BoxSuperiore">
-            <p class="intestazione">NUOVO EVENTO</p>
-            <div id="demo_descrizione"></div>
-            <form class="NuovoNegozio" action="" method="post" onsubmit="return descrizione()" >
-                <div>
-                <label for="evento">Tipo di evento:</label>
-                <select name="evento" id="evento" class="allarga">
-                <option value="Cerca nel menu:">Cerca nel menu:</option>
-                <option value="Apertura">Apertura</option>
-                <option value="Chiusura">Chiusura</option>
-                <option value="Novita">Novita</option>
-                </select> 
-                <label for="testo">Descrizione:</label>
-                <textarea id="testo" name="testo" rows="4" cols="36" >Inserisci nuovo evento</textarea>
-                <input title="Tasto Reset" type="reset" value="Reset"/>  
-                <input title="Tasto Salva" type="submit" value="Salva"/>
-                </div>
-            </form>
-    </div>
+        <div id="sotto">
+
+            <div class="form_sotto">
+                <p class="intestazione">APERTURA/CHIUSURA</p>
+                <div id="demo_descrizione"></div>
+                <form  action="" method="post" onsubmit="return descrizione()" >
+                  <div >
+                   <label for="evento">Tipo di evento:</label>
+                   <select name="evento" id="evento">
+                   <option value="Cerca nel menu:">Cerca nel menu:</option>
+                   <option value="Apertura">Apertura</option>
+                   <option value="Chiusura">Chiusura</option>
+                   </select> 
+                   <label for="data">Inserire Data:</label>
+                   <input name="data" id="data" type="text" />  
+                   <input name="tasto_salva" type="submit" value="Salva"/>
+                  </div>
+                </form>
+            </div>
+
+            <div class="form_sotto">
+                <p class="intestazione">NOVITA'</p>
+                <form  action="" method="post">
+                   <div>
+                   <label for="novità">Descrizione:</label>
+                   <textarea id="novità" name="novità" rows="4" cols="36" >Inserisci la nuova novità</textarea>
+                   <input type="reset" name="tasto_reset"  value="Reset"/>  
+                   <input type="submit" name="tasto_salva"  value="Salva"/>
+                   </div>
+                </form>
+            </div>
+         
     
-        <div class="BoxSuperiore">
-            <p class="intestazione">MODIFICA PASSWORD</p>
-            <div id="demos"></div>
-            <form class="NuovoNegozio" action="" method="post" onsubmit="return validateForm_1(this)" >
-                <div>
-                <label for="password">Password:</label>
-                <input name="password" class="allarga" type="password" />
-                <label for="password">Conferma Password:</label>
-                <input name="Password_1" class="allarga" type="password" />   
-                                   
-                <input title="Tasto Reset" type="reset" value="Reset"/>  
-                <input title="Tasto Salva" type="submit" value="Salva"/>
-                </div>
-            </form>
+            <div class="form_sotto">
+                <p class="intestazione">MODIFICA PASSWORD</p>
+                <div id="demos"></div>
+                <form  action="" method="post" onsubmit="return validateForm_1(this)" >
+                 <div>
+                  <label for="nuova_password">Password:</label>
+                  <input type="password" name="nuova_password" id="nuova_password" />
+                  <label for="conferma_nuova_password">Conferma Password:</label>
+                  <input type="password" name="conferma_nuova_password" id="conferma_nuova_password" />  
+                  <div class="invia">   
+                  <input name="Tasto Reset" type="reset" value="Reset"/>  
+                  <input name="Tasto Salva" type="submit" value="Salva"/>
+                  </div> 
+                 </div>
+                </form>
+            </div>
+
         </div>
+          
     </div>
-        
-        
-    </div>
+
 <?php
+$controller->footer();
 ?>
