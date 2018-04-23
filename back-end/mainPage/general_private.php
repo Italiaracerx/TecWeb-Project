8e79696a59324c6eca4fb6f74f719052acf780c5
Daniel Rossi <bovo.96@gmail.com>
2018-04-23 16:31:01 +0200

    inserimento cartella immagini e pagine parte privata

--- back-end/mainPage/general_private.php
+++ back-end/mainPage/general_private.php
@@ -1,8 +1,9 @@
 <?php
     require_once __DIR__.'/../class/sistema/controller.php';
-    require_once __DIR__.'/../class/pagine/generalPrivatePage.php';
     require_once __DIR__.'/../class/pagine/page_private.php';
     require_once __DIR__.'/../class/query/log.php';
+    require_once __DIR__.'/../class/pagine/menu/menu.php';
+    require_once __DIR__.'/../class/pagine/menu/staticMenu.php';
 
 
     $controller = new controller(new page_private('Generale',(new menu((new staticPath())->user())), 'general_private'));
@@ -10,106 +11,154 @@
     $controller->head();
 ?>
     <div id="content">
-        <div id="sopra">
 
-            <div class="form_sopra">
-              <p class="intestazione">CREAZIONE NEGOZIO</p>
-              <div id="demo"></div>
-              <form  action="mainForm/newUser.php" method="post" onsubmit="return validateForm(this)">
-                <div>
-                <label for="nome_negozio">Nome Negozio:</label>
-                <input type="text" id="username" name="username"/>    
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" />
-                <label for="conferma_password">Conferma Password:</label>
-                <input type="password" id="conferma_password" name="conferma_password" />  
-                <div class="invia">                       
-                <input type="reset"  name="tasto_reset"  value="Reset"/>  
-                <input type="submit" name="tasto_salva"  value="Salva"/>
-                </div>
-                </div>
-               </form>
+<div id="content_generale">
+    <h3 id="titolo_generale">GENERALE</h3>
+    <div id="sopra">
+
+         <div class="form_sopra">
+             <p class="intestazione">MODIFICA CONTATTI</p>
+             <div id="verifica_sito"></div> <div id="verifica_tel"></div> <div id="verifica_mail"></div>
+             <form action="" method="post" onsubmit="return checkEmail(this)" >
+             <div>
+               <label for="Telefono">Telefono / Fax:</label>
+               <input type="text" name="Telefono" id="Telefono" value="041610265"/>
+               <label for="Email">Email:</label>
+               <input type="text" name="Email" id="Email" value="blablabla@lego.com" />
+               <label for="Sito_web">Sito web:</label>
+               <input type="text" name="Sito_web" id="Sito_web" value="www.lego.com/it-it" />
+               <div class="invia">   
+               <input type="reset" value="Reset"/>  
+               <input type="submit" value="Salva"/>    
+               </div>                           
+             </div>                     
+             </form>
+         </div>
+
+         <div class="form_sopra">
+             <p class="intestazione">MODIFICA PASSWORD</p>
+             <div id="verifica_passw"></div>
+             <form  action="" method="post" onsubmit="return validateForm(this)">
+             <div>
+               <label for="password">Password:</label>
+               <input type="password" name="password" id="password"/>
+               <label for="conferma_password">Conferma Password:</label>
+               <input type="password" name="conferma_password" id="conferma_password"/> 
+               <div class="invia">              
+               <input type="reset"  value="Reset"/>  
+               <input type="submit" value="Salva"/>   
+               </div>                             
+             </div>
+             </form>
+         </div>
+
+ </div>            
+
+ <div id="parte_centrale">  
+
+         <div class="form_centrali">
+         <p class="intestazione">MODIFICA LOGO</p>
+           <form action="" method="post" enctype="multipart/form-data">
+               <div>
+               <label for="carica_immmagine">Selezionare un file:</label>
+               <input type="file" name="immagine" id="carica_immmagine"/>
+               <input type="submit" value="Salva"/>
+               </div>
+          </form>
+         </div>
+
+         <div class="form_centrali">
+         <p class="intestazione">MODIFICA NOME NEGOZIO</p>
+             <div id="verifica_nomenegozio"></div>
+             <form   action="" method="post" onsubmit="return negozio(this)" > 
+                 <div id="modifica_nome">
+                 <label for="nome_negozio">Nome:</label>
+                 <input type="text" name="nome_negozio" id="nome_negozio" value="LEGO STORE" />
+                 <input type="submit" value="Salva"/>
+                 </div>
+             </form>
+         </div>
+ </div>
+
+ <div id="sotto">
+
+     <div id="sinistra">
+         <p class="intestazione">MODIFICA ORARIO</p>
+         <p id="esempio">Il centro apre alle 08:30 </br> chiude alle 21:30  </p>
+
+         <div id="verifica_orario"></div>
+         <form action="" method="post" onsubmit="return checkorario(this)">
+
+             <div class="orario">
+             <label for="lunedì"> Lunedì :</label>
+             <input type="text" name="lunedi" id="lunedì" maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="uno"></div>
              </div>
 
-            <div class="form_sopra">
-              <p class="intestazione">ELIMINA NEGOZIO</p> 
-              <div id="demo_1"></div>
-              <form  action="mainForm/deleteUser.php" method="post" onsubmit="return validateUser()">
-                <div>
-                  <label for="elimina_negozio">Nome Negozio:</label>
-                  <select name="nelimina_negozio" id="elimina_negozio" >
-                  <option value="Cerca nel menu:">Cerca nel menu:</option>
-                    <?php 
-                        $log = new login();
-                        $lista_utenti =$log->read();
-                        foreach ($lista_utenti as $key) {
-                            echo '<option value="'.$key['username'].'">'.$key['username'].'</option>';           
-                        }
-                    ?>
-                  </select>  
-                  <div class="invia">                
-                  <input type="reset"  name="tasto_reset"  value="Reset"/>  
-                  <input type="submit" name="tasto_salva"  value="Salva"/>
-                  </div>
-                </div>
-               </form>
-            </div>
+             <div class="orario">
+             <label for="martedì"> Martedì :</label>
+             <input type="text" name="martedi" id="martedì"  maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="due"></div>
+             </div>
 
-         </div>
-     
-        <div id="sotto">
+             <div class="orario">
+             <label for="mercoledì" > Mercoledì :</label>
+             <input type="text" name="mercoledi" id="mercoledì"  maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="tre"></div>
+             </div>
+             
+             <div class="orario">
+             <label for="giovedì" > Giovedì :</label>
+             <input type="text" name="giovedi" id="giovedì" maxlength="11" onkeypress="return onlyNumeric(event);" />
+             <div id="quattro"></div>
+             </div>
 
-            <div class="form_sotto">
-                <p class="intestazione">APERTURA/CHIUSURA</p>
-                <div id="demo_descrizione"></div>
-                <form  action="" method="post" onsubmit="return descrizione()" >
-                  <div >
-                   <label for="evento">Tipo di evento:</label>
-                   <select name="evento" id="evento">
-                   <option value="Cerca nel menu:">Cerca nel menu:</option>
-                   <option value="Apertura">Apertura</option>
-                   <option value="Chiusura">Chiusura</option>
-                   </select> 
-                   <label for="data">Inserire Data:</label>
-                   <input name="data" id="data" type="text" />  
-                   <input name="tasto_salva" type="submit" value="Salva"/>
-                  </div>
-                </form>
-            </div>
+             <div class="orario">
+             <label for="venerdì" > Venerdì :</label>
+             <input type="text" name="venerdi" id="venerdì"  maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="cinque"></div>
+             </div>
 
-            <div class="form_sotto">
-                <p class="intestazione">NOVITA'</p>
-                <form  action="" method="post">
+             <div class="orario">
+             <label for="sabato" > Sabato :</label>
+             <input  type="text" name="sabato" id="sabato"  maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="sei"></div>
+             </div>
+
+             <div class="orario">
+             <label for="domenica" > Domenica :</label>
+             <input type="text" name="domenica"  id="domenica" maxlength="11" onkeypress="return onlyNumeric(event);"/>
+             <div id="sette"></div>
+             </div>                        
+              <div class="pulsante">
+             <input  type="submit" value="Salva"/>                                
+             <input  type="reset" value="Reset"/>  
+             </div> 
+         </form>
+     </div>
+
+     <div id="destra">
+         <div id="descrizione">
+             <p class="intestazione">MODIFICA DESCRIZIONE</p>
+             <div id="verifica_descrizione"></div>
+             <form action="" method="post" onsubmit="return descrizione(this)">
                    <div>
-                   <label for="novità">Descrizione:</label>
-                   <textarea id="novità" name="novità" rows="4" cols="36" >Inserisci la nuova novità</textarea>
-                   <input type="reset" name="tasto_reset"  value="Reset"/>  
-                   <input type="submit" name="tasto_salva"  value="Salva"/>
-                   </div>
-                </form>
-            </div>
-         
-    
-            <div class="form_sotto">
-                <p class="intestazione">MODIFICA PASSWORD</p>
-                <div id="demos"></div>
-                <form  action="mainForm/changePassword.php" method="post" onsubmit="return validateForm_1(this)" >
-                 <div>
-                  <label for="nuova_password">Password:</label>
-                  <input type="password" name="nuova_password" id="nuova_password" />
-                  <label for="conferma_nuova_password">Conferma Password:</label>
-                  <input type="password" name="conferma_nuova_password" id="conferma_nuova_password" />  
-                  <div class="invia">   
-                  <input name="Tasto Reset" type="reset" value="Reset"/>  
-                  <input name="Tasto Salva" type="submit" value="Salva"/>
-                  </div> 
-                 </div>
-                </form>
-            </div>
+                      <label for="motto">Motto:</label>                                          
+                      <textarea name="testo_motto" id="motto" rows="2" cols="50">Our mission: To inspire and develop the builders of tomorrow</textarea>  
+                      <label for="descrizione_negozio">Descrizione:</label>                     
+                      <textarea name="testo_descrizione" id="descrizione_negozio" rows="5" cols="50">Il nostro scopo è ispirare ed educare i bambini a pensare creativamente, ragionare in modo sistematico e realizzare il loro potenziale, plasmando il loro futuro e sperimentando le infinite possibilità umane. </textarea>  
+                      <div class="pulsante">
+                      <input  type="submit" value="Salva"/>                                
+                      <input  type="reset" value="Reset"/>   
+                      </div>  
+                 </div>           
+             </form>
+         </div>
+     </div>
 
-        </div>
-          
-    </div>
+ </div>
+</div>
+</div>
 
 <?php
 $controller->footer();
---
+++ back-end/mainPage/prodotti_private.php
@@ -0,0 +1,54 @@
+<?php
+    require_once __DIR__.'/../class/sistema/controller.php';
+    require_once __DIR__.'/../class/pagine/page_private.php';
+    require_once __DIR__.'/../class/query/log.php';
+    require_once __DIR__.'/../class/pagine/menu/menu.php';
+    require_once __DIR__.'/../class/pagine/menu/staticMenu.php';
+
+
+    $controller = new controller(new page_private('Promozioni',(new menu((new staticPath())->user())), 'general_private'));
+    $controller->check_session();
+    $controller->head();
+?>
+    <div id="content">
+        <div id="content_promozioni">
+            <div id="colonna_sinistra">
+                    <p class="intestazione">NUOVO PRODOTTO</p>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div>
+                    	<label for="nome_prodotto">Nome:</label>
+                        <input type="text" name="nome_prodotto" id="nome_prodotto" value="Star-Wars 75104" />
+
+                        <label for="campo_alt">Alternativa testuale dell'immagine del prodotto:</label>
+                        <input type="text" name="campo_alt" id="campo_alt"/>
+
+                        <label for="descrizione_prodotto">Descrizione prodotto:</label>
+                        <textarea name="descrizione_prodotto" id="descrizione_prodotto" rows="10" cols="30"></textarea>
+                        
+                        <label for="seleziona_promozione">Seleziona il prodotto da inserire:</label>
+                        <input type="file" name="immagine" id="seleziona_promozione"/>
+                        <div class="invia">
+                        <input  type="submit" value="Salva"/>   
+                        </div>
+
+                       </div>                                                     
+                    </form>
+            </div>
+            
+            <div id="colonna_destra">
+                <p class="intestazione">PRODOTTI CORRENTI</p>
+                <div class="imgPromozione">
+                    <img class="promozione" src="images/lego1.jpg" alt="promozione"/> 
+                </div>
+                <div class="imgPromozione">
+                    <img class="promozione" src="images/lego2.jpg" alt="promozione"/> 
+                </div>
+                <div class="imgPromozione">
+                    <img class="promozione" src="images/lego3.jpg" alt="promozione"/> 
+                </div>
+            </div>            
+        </div>
+    </div>
+<?php
+$controller->footer();
+?>
