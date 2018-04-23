a9d00e167e01900e50ae686276b7e0ca0dfd1a2f
Manfredi Smaniotto <manfri96@live.it>
2018-04-23 16:00:27 +0200

    dubbio tentativo di modifica inserimento immagini

--- back-end/mainPage/mainForm/insertImage.php
+++ back-end/mainPage/mainForm/insertImage.php
@@ -7,8 +7,7 @@
         $description=$_POST['text'];
     }
 
-    //////////////////////////////////////  SOSTITUISCI QUì
-    $image=new image($_SESSION['user'], $_SESSION['page'], $description, $_FILES['immagine']['tmp_name'], strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION)))
+    $image=new image($_SESSION['user'], $_SESSION['link'], $description, $_FILES['immagine']['tmp_name'], strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION)))
     //$image=new image($_SESSION["cod"], $_SESSION['flag_text'], $description, $_FILES["fileToUpload"]["tmp_name"], strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
     $image.checker(isset($_POST["submit"]), $_FILES["fileToUpload"]["size"]);
     $controller=new controller($image);
