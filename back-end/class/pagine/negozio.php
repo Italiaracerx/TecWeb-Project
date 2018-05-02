1c0aa7884e220028d3cf3490dd7c8804e680f0af
Daniel Rossi <bovo.96@gmail.com>
2018-05-02 23:00:53 +0200

    Creazione delle pagine negozi
    pagina negozi type e pagina negozi content

---
+++ back-end/class/pagine/negozio.php
@@ -0,0 +1,68 @@
+<?php 
+require_once __DIR__.'/../interfacce/type_page.php';
+
+
+class page_public implements type_page{
+	private static $style="style.css";
+	private $menu;
+    private $name;
+    private $shop;
+
+	public function __construct(){
+		$this->shop =NULL;
+        if($_GET){
+            $this->shop =': '.$_GET['negozio'];
+        }
+		$this->name ='negozio';
+		$this->menu = new menu((new staticPath())->public_menu('1'));
+	}
+	public function intestazione(){
+		$file = file_get_contents('../../class/HTMLstored/public/preambolo.html', FILE_USE_INCLUDE_PATH);
+		$file = str_replace('__USER__',$_SESSION['user'],$file);
+		$file = str_replace('__NAME_PAGE__',$this->name,$file);
+		$file = str_replace('__STYLE__',page_public::$style,$file);
+
+		echo $file;
+    }
+    public function header(){
+        $file = file_get_contents('../../class/HTMLstored/public/header.html', FILE_USE_INCLUDE_PATH);
+        echo $file;
+	}
+	public function menu(){
+        echo '<div id="menu">';
+		$this->menu->print();
+	}		
+    public function breadcrumb(){
+    	echo '	<div id="breadcrumb">
+        			<h2><strong>'.$this->name.'</strong>'.$this->shop.'</h2>        
+    			</div>';
+    }
+    public function footer(){
+        echo '       
+        <div id="footer">
+            <div id="footerMenu">
+                <div id="contentMenuFooter">';
+                    $this->menu->print();
+                echo '
+            </div>
+        </div>
+
+            <h3>Centro Commerciale Archimede</h3>
+            <img id="logoFooter" alt="logofooter" src="images/logo.jpg"/>
+            <div id="infoFooter">
+            <p>Via Trieste, 63  | 35121 Padova (<span xml:lang="en">Italy</span>)| Telefono: +39 049 827 1200 | <span xml:lang="en">e-mail</span>:info@centro.archimede.it</p>
+			</div> <!-- fine contatti_footer--></div>
+			</body>
+			</html>';
+	}
+    public function body(){
+		$this->header();
+		$this->menu();
+		$this->breadcrumb();
+    }
+	public function head(){
+		$this->intestazione();
+		$this->body();
+	}
+}
+?>
