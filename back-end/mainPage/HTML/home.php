77b16ada3e23e8e27b3fbe08c5b3539cf68d4b4e
Daniel Rossi <bovo.96@gmail.com>
2018-05-08 20:41:25 +0200

    fix bug vari

--- back-end/mainPage/HTML/home.php
+++ back-end/mainPage/HTML/home.php
@@ -7,7 +7,7 @@
 
 
     $controller = new controller();
-    $controller->setPage(new page_public('Generale',(new menu((new staticPath())->public_menu('0')))));
+    $controller->setPage(new page_public('home',(new menu((new staticPath())->public_menu('0')))));
     $controller->head();
 ?>
   <div id="content">
