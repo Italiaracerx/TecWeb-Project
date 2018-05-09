b86922e357a8bf7254afbf00b1d0ef6a551e90c5
Daniel Rossi <bovo.96@gmail.com>
2018-05-09 14:58:50 +0200

    stampa immagini pagina promozioni

--- back-end/mainPage/HTML/promozioni.php
+++ back-end/mainPage/HTML/promozioni.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__.'/../../class/sistema/controller.php';
     require_once __DIR__.'/../../class/pagine/page_public.php';
-    require_once __DIR__.'/../../class/query/log.php';
+    require_once __DIR__.'/../../class/query/image.php';
     require_once __DIR__.'/../../class/pagine/menu/menu.php';
     require_once __DIR__.'/../../class/pagine/menu/staticMenu.php';
 
@@ -12,29 +12,33 @@
 ?>
     <div id="content">
         <h2 id="intestazione">PROMOZIONI CORRENTI</h2>
-        <div id="PromozioniAttive">
-                <div class="promozioneHome">
-                    <img class="promozione" src="images/promo1.jpg" alt="promozione"/> 
-                    <p>Nome_Negozio_1</p>
-                </div>
-                <div class="promozioneHome">
-                    <img class="promozione" src="images/promo2.jpg" alt="promozione"/> 
-                    <p>Nome_Negozio_2</p>
-                </div>
-                <div class="promozioneHome">
-                    <img class="promozione" src="images/promo3.png" alt="promozione"/> 
-                    <p>Nome_Negozio_3</p>
-                </div>
-                <div class="promozioneHome">
-                    <img class="promozione" src="images/promo1.jpg" alt="promozione"/> 
-                    <p>Nome_Negozio_4</p>
-                </div>
-                <div class="promozioneHome">
-                    <img class="promozione" src="images/promo1.jpg" alt="promozione"/> 
-                    <p>Nome_Negozio_4</p>
-                </div>
-                
-        </div>    
+        <?php
+            $promozione = new image('promozione');
+            $result_promozione =$promozione->read();
+                    $rows =array();
+                    while($row = $result_promozione->fetch_array(MYSQLI_ASSOC)){
+                        $rows[] = $row;
+                    }
+                    if(!count($rows)){
+                        echo '                
+                        <div class ="no_image">
+                            <p class="text_message">coming soon</p>
+                        </div>';
+                    }
+                    else{
+                        echo '<div id="promozioniAttive">';
+                        foreach($rows as $row){
+                            echo '
+                            <div class="singola_promozione">
+                                <a href="promo2.html">
+                                    <img class="promozione" src="images/promozione/'.$row['source'].'" alt="'.$row['alt'].'"/>
+                                </a> 
+                                <p>'.$row['negozio'].'</p>
+                            </div>';
+                        }
+                        echo '  </div>';
+                    }
+        ?>
     </div>
     
 
