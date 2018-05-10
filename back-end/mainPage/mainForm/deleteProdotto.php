a519c494a926b9770092833b176e34f8d1680896
Daniel Rossi <bovo.96@gmail.com>
2018-05-10 16:16:10 +0200

    correzione upload immagini

--- back-end/mainPage/mainForm/deleteProdotto.php
+++ back-end/mainPage/mainForm/deleteProdotto.php
@@ -3,7 +3,7 @@
 	require_once __DIR__.'/../../class/query/image.php';
 
 	$controller = new controller_query();
-	$prodotto =new image('prodotto');
+	$prodotto =new image('prodotto',$_SESSION['user']);
 	$controller->setQuery($prodotto);
 	$controller->delete();
 ?>
--- back-end/mainPage/mainForm/deletePromozione.php
+++ back-end/mainPage/mainForm/deletePromozione.php
@@ -3,7 +3,7 @@
 	require_once __DIR__.'/../../class/query/image.php';
 
 	$controller = new controller_query();
-	$promozione =new image('promozione');
+	$promozione =new image('promozione',$_SESSION['user']);
 	$controller->setQuery($promozione);
 	$controller->delete();
 ?>
